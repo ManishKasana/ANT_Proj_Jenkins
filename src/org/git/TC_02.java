fabc7563afebc028057217b9a51994c0246a72fc
ManishKasana <manishkasana@gmail.com>
2017-10-08 16:49:42 +0530

    first commit

---
+++ src/org/git/TC_02.java
@@ -0,0 +1,18 @@
+package org.git;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_02 {
+	WebDriver driver;
+	@Test
+	public void logic(){
+		System.setProperty("webdriver.chrome.driver","D:/Selenium Set_Up/Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("https://www.google.com");
+		
+
+}
+
+}
